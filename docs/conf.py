c6c7b37b85d3f6e392c22035a1d6c0f763ef0b6c
imitxelena <ifrentzua@gmail.com>
2020-02-10 18:21:45 +0100

    new5

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'classic'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
